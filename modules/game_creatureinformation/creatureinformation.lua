bdf0bc299ca6740ed65f9a0403fb179eb19b5d30
Nokturno <35547755+nok1111@users.noreply.github.com>
2025-02-28 09:20:22 -0600

    creature information

--- modules/game_creatureinformation/creatureinformation.lua
+++ modules/game_creatureinformation/creatureinformation.lua
@@ -1,3 +1,30 @@
+local playerTitles = {
+
+["Nok"] = {title = "[Administrator]", color = "alpha"}
+
+}
+
+local npcTitles = {
+["Khadin"] = {title = "[Ressource Shop]", color = "#f5c367", quest = true},
+["Armin"] = {title = "[Ancestral Task]", color = "#677ef5"},
+["Magnus Blackwater"] = {title = "[Captain of Ships]", color = "#67caf5"},
+["Shipwright Trader"] = {title = "[Ship Parts]", color = "#a68556"},
+["Alaistar"] = {title = "[Potion-Ressources]", color = "#e5f280"},
+["Alesar"] = {title = "[Djinn Seller]", color = "#e5f280"},
+["Alexander"] = {title = "[Magician Shop]", color = "#e5f280"},
+["Alice"] = {title = "[Blessing Seller]", color = "#e5f280", quest = true},
+
+}
+
+local creatureTitles = {
+["Avarithia"] = {title = "[The Goddess of Greed]", color = "#FFD700"},
+["Al-Razi"] = {title= "[The Void Alchemist]", color = "#FFFFFF"},
+["Gor'kaal"] = {title= "[Dunefang Clan]", color = "#FFDE21"},
+["Drekhul"] = {title= "[Dunefang Clan]", color = "#FFDE21"},
+["Drakkarim"] = {title= "[Dunefang Clan]", color = "#FFDE21"},
+["Gor'zul"] = {title= "[Dunefang Clan]", color = "#FFDE21"}
+}
+
 controller = Controller:new()
 
 if not g_gameConfig.isDrawingInformationByWidget() then
@@ -12,6 +39,76 @@ local debug = false
 local COVERED_COLOR = '#606060'
 local NPC_COLOR = '#66CCFF'
 
+local function setCreatureTitle(creature)
+    local name = creature:getName()
+    local titleFont = "verdana-11px-rounded"
+
+    -- Get the creature's existing widget
+    local widget = creature:getWidgetInformation()
+    if not widget then return end -- Safety check
+
+    -- Remove old title if it exists
+    if widget.titleWidget then
+        widget.titleWidget:destroy()
+        widget.titleWidget = nil
+    end
+
+    -- Create the title widget
+    local titleWidget = g_ui.createWidget('Panel')
+    titleWidget:setFont(titleFont)
+    titleWidget:setTextAutoResize(true)
+    titleWidget:setColor('white')
+    titleWidget:setFontScale(1)
+    titleWidget:setMarginBottom(30) -- Adjust height above name
+    titleWidget:setMarginRight(50)  -- Adjust horizontal position
+
+    -- Store the titleWidget in the creature's widget
+    widget.titleWidget = titleWidget
+
+    -- Assign title if applicable
+    if creature:isPlayer() and playerTitles[name] then
+        titleWidget:setText(playerTitles[name].title)
+        titleWidget:setBackgroundColor(playerTitles[name].color)
+        creature:attachWidget(titleWidget)
+
+    elseif creature:isNpc() and npcTitles[name] then
+        titleWidget:setText(npcTitles[name].title)
+        titleWidget:setBackgroundColor(npcTitles[name].color)
+        creature:attachWidget(titleWidget)
+
+        -- Add quest effect if applicable
+        if npcTitles[name].quest then
+            creature:attachEffect(g_attachedEffects.getById(31))
+        end
+
+    elseif creatureTitles[name] then -- Assume all other creatures
+        titleWidget:setText(creatureTitles[name].title)
+        titleWidget:setBackgroundColor(creatureTitles[name].color)
+        creature:attachWidget(titleWidget)
+
+    else
+        titleWidget:destroy() -- Remove if no title is found
+    end
+end
+
+
+local function onDisappear(creature)
+    local widget = creature:getWidgetInformation()
+    if widget and widget.titleWidget then
+        widget.titleWidget:destroy()
+        widget.titleWidget = nil
+    end
+end
+
+local function onAppear(creature)
+    -- Ensure the creature has a widget
+    local widget = creature:getWidgetInformation()
+    if widget then
+        -- Reapply the title
+        setCreatureTitle(creature)
+    end
+end
+
 local function onCreate(creature)
     local widget = g_ui.loadUI('creatureinformation')
 
@@ -23,13 +120,9 @@ local function onCreate(creature)
     end
 
     widget.manaBar:setVisible(creature:isLocalPlayer())
-	widget:setImageSource('/images/monsterbar/basic_monster_background2')
-	widget:setImageSize({
-            width = 50,
-            height = 12
-        })
+	
     creature:setWidgetInformation(widget)
-end
+    end
 
 local function onHealthPercentChange(creature, healthPercent, oldHealthPercent)
     local gameMapPanel = modules.game_interface.getMapPanel()
@@ -51,9 +144,16 @@ local function onHealthPercentChange(creature, healthPercent, oldHealthPercent)
     end
 
     widget.name:setColor(color)
+
+          -- Set a background image for the health bar
+    --widget.lifeBar:setImageSource('/images/monsterbar/basic_monster_background2')
+    --widget.lifeBar:setImageSize({ width = 50, height = 12 })
+    
     widget.lifeBar:setPercent(healthPercent)
     widget.lifeBar:setBackgroundColor(color)
     widget.lifeBar:setVisible(gameMapPanel:isDrawingHealthBars())
+
+
 end
 
 local function onManaChange(player, mana, maxMana, oldMana, oldMaxMana)
@@ -79,6 +179,9 @@ local function onChangeName(creature, name, oldName)
 
     infoWidget.name:setText(name)
     infoWidget.name:setVisible(gameMapPanel:isDrawingNames())
+    -- Attach title using the new function
+    --setCreatureTitle(creature)
+
 end
 
 local function onCovered(creature, isCovered, oldIsCovered)
@@ -167,6 +270,8 @@ local creatureEvents = {
     onSkullChange = function(creature, id) setIcon(creature, id, getSkullImagePath, 'skull') end,
     onShieldChange = function(creature, id) setIcon(creature, id, getShieldImagePathAndBlink, 'shield') end,
     onEmblemChange = function(creature, id) setIcon(creature, id, getEmblemImagePath, 'emblem') end,
+    onDisappear = onDisappear, -- Remove only the title
+    onAppear = onAppear -- Re-add the title when the creature reappears
 };
 
 function toggleInformation()
@@ -184,11 +289,13 @@ function toggleInformation()
     end
 end
 
-function controller:onGameStart()
+function controller:onInit()
     controller:registerEvents(Creature, creatureEvents)
     controller:registerEvents(LocalPlayer, { onManaChange = onManaChange })
+end
 
-    if devMode then
+ if devMode then
+    function controller:onGameStart()
         local spectators = modules.game_interface.getMapPanel():getSpectators()
         for _, creature in ipairs(spectators) do
             onCreate(creature)
@@ -211,3 +318,5 @@ function controller:onGameStart()
         end
     end
 end
+
+
