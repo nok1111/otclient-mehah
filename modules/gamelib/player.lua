96b5fed30411fd0167c642d788003c7091fed4ef
Nokturno <35547755+nok1111@users.noreply.github.com>
2025-05-13 20:43:16 -0500

    rune slots system

--- modules/gamelib/player.lua
+++ modules/gamelib/player.lua
@@ -51,9 +51,12 @@ InventorySlotFeet = 8
 InventorySlotFinger = 9
 InventorySlotAmmo = 10
 InventorySlotPurse = 11
+InventorySlotRune1 = 12
+InventorySlotRune2 = 13
+InventorySlotRune3 = 14
 
 InventorySlotFirst = 1
-InventorySlotLast = 10
+InventorySlotLast = 14
 
 function Player:isPartyLeader()
     local shield = self:getShield()
--- src/client/const.h
+++ src/client/const.h
@@ -103,9 +103,9 @@ namespace Otc
         InventorySlotRing,
         InventorySlotAmmo,
         InventorySlotPurse,
-        InventorySlotExt1,
-        InventorySlotExt2,
-        InventorySlotExt3,
+        InventorySlotRune1,
+        InventorySlotRune2,
+        InventorySlotRune3,
         InventorySlotExt4,
         LastInventorySlot
     };
