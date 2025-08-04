902f1eaad59784c859694e0df757b238dcdcef9a
Renato Machado <mehahx@gmail.com>
2025-08-03 21:48:50 -0300

    cleanup: texture atlas

--- src/framework/ui/uiwidget.h
+++ src/framework/ui/uiwidget.h
@@ -603,7 +603,7 @@ protected:
     std::vector<std::pair<Color, CoordsBufferPtr>> m_colorCoordsBuffer;
 
     float m_fontScale{ 1.f };
-    AtlasRegion* m_lastAtlasRegion = nullptr;
+    const AtlasRegion* m_atlasRegion = nullptr;
 
 public:
     void resizeToText();
