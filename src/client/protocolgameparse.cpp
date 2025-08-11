549a083cf9c4758d8508423ed43a87011549a5ae
Luan Luciano <luanluciano@outlook.com>
2025-08-11 15:17:58 -0300

    fix: message mode logging in parseTextMessage

--- src/client/protocolgameparse.cpp
+++ src/client/protocolgameparse.cpp
@@ -2521,7 +2521,7 @@ void ProtocolGame::parseTextMessage(const InputMessagePtr& msg)
     const Otc::MessageMode mode = Proto::translateMessageModeFromServer(code);
     std::string text;
 
-    g_logger.debug("[ProtocolGame::parseTextMessage] code: {}, mode: {}", code, code);
+    g_logger.debug("[ProtocolGame::parseTextMessage] code: {}, mode: {}", code, mode);
 
     switch (mode) {
         case Otc::MessageChannelManagement:
