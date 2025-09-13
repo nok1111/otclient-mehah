b24aebcfb9d3ffe867fa2286e63b1d2dee7e3c96
Nokturno <35547755+nok1111@users.noreply.github.com>
2025-09-13 17:53:49 -0500

    Revert "Update init.lua"
    This reverts commit 7a229f03e92bb5dbec0a77b53e415a6b5f8eede8.

--- init.lua
+++ init.lua
@@ -9,15 +9,20 @@ Services = {
     createAccount = "http://23.191.72.22/clientcreateaccount.php", --./client_entergame -- createAccount.lua
 }
 
-
+--[[
 Servers_init = {
-    ["23.191.72.22"] = {
+    ["http://127.0.0.1/login.php"] = {
+        ["port"] = 80,
+        ["protocol"] = 1320,
+        ["httpLogin"] = true
+    },
+    ["ip.net"] = {
         ["port"] = 7171,
-        ["protocol"] = 1098,
+        ["protocol"] = 860,
         ["httpLogin"] = false
-    }
+    },
 }
-
+]]
 
 g_app.setName("OTClient - Redemption");
 g_app.setCompactName("otclient");
