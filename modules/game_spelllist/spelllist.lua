e0c3adba6da33ed54dcbd957dea7dd9d21352c1e
Nokturno <35547755+nok1111@users.noreply.github.com>
2026-08-30 22:00:38 -0500

    colored text

--- modules/game_spelllist/spelllist.lua
+++ modules/game_spelllist/spelllist.lua
@@ -324,7 +324,7 @@ function updateSpellInformation(widget)
 
     -- New UI only
     if descriptionValueLabel then
-        descriptionValueLabel:setText(tr(description))
+        descriptionValueLabel:setColoredText(tr(description))
     else
         print('[SpellList] WARN: descriptionValueLabel not found')
     end
